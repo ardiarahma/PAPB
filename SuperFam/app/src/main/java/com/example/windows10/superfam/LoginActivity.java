48ff5a8610ffc78377b5ff1abd0dad62fa6d9203
nurardiar@gmail.com <nurardiar@gmail.com>
2018-05-03 14:07:25 +0700

    ganti fragment ke activity

--- SuperFam/app/src/main/java/com/example/windows10/superfam/LoginActivity.java
+++ SuperFam/app/src/main/java/com/example/windows10/superfam/LoginActivity.java
@@ -2,15 +2,12 @@ package com.example.windows10.superfam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
-    TextView
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_login);
     }
 }
