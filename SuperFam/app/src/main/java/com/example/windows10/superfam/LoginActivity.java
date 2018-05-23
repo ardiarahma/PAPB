3fe5f80759d55a8b3fe873d4ee44446afec80cc1
nurardiar@gmail.com <nurardiar@gmail.com>
2018-05-23 21:08:38 +0700

    baru dan ga error

--- SuperFam/app/src/main/java/com/example/windows10/superfam/LoginActivity.java
+++ SuperFam/app/src/main/java/com/example/windows10/superfam/LoginActivity.java
@@ -1,13 +1,22 @@
 package com.example.windows10.superfam;
 
+
 import android.os.AsyncTask;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.chabbal.slidingdotsplash.ImageViewPagerAdapter;
+import com.chabbal.slidingdotsplash.OnSetImageListener;
+import com.chabbal.slidingdotsplash.SlidingSplashView;
+import com.chabbal.slidingdotsplash.ViewPagerAdapter;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,46 +25,48 @@ import java.util.ArrayList;
 
 import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity{
 
-    EditText email, password, fullname;
-    Button login, register;
-    TabLayout tabLayout;
-    ViewPager mImageViewPager;
+//    EditText email, password, fullname;
+//    Button login, register;
+//    TabLayout tabLayout;
+//    //PagerAdapter mImageViewPager;
 
-    String URL = "http://localhost/server/index.php";
+//    String URL = "http://localhost/server/index.php";
 
-    JSONParser jsonParser = new JSONParser();
+//    JSONParser jsonParser = new JSONParser();
 
-    int i = 0;
+//    int i = 0;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+
+//        //splashDots
+//        SlidingSplashView splashView  = (SlidingSplashView) findViewById(R.id.splash);
+//        splashView.addOnPageChangeListener(this);
 
 
-        mImageViewPager = (ViewPager) findViewById(R.id.pager);
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabDots);
-        tabLayout.setupWithViewPager(mImageViewPager, true);
+        //login and register
 
-        email = (EditText) findViewById(R.id.email);
-        password = (EditText) findViewById(R.id.password);
+//        email = (EditText) findViewById(R.id.email);
+//        password = (EditText) findViewById(R.id.password);
 
-        login = (Button) findViewById(R.id.login);
-        register = (Button) findViewById(R.id.register);
+//        login = (Button) findViewById(R.id.login);
+//        register = (Button) findViewById(R.id.register);
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AttemptLogin attemptLogin = new AttemptLogin();
-                attemptLogin.execute(email.getText().toString(), password.getText().toString(), "");
-            }
-        });
+//        login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                AttemptLogin attemptLogin = new AttemptLogin();
+//                attemptLogin.execute(email.getText().toString(), password.getText().toString(), "");
+//            }
+//        });
 
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+//        register.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
 
                // if (i == 0) {
                //     i = 1;
@@ -69,63 +80,80 @@ public class LoginActivity extends AppCompatActivity {
                //     login.setVisibility(View.VISIBLE);
                //     i = 0;
 
-                    AttemptLogin attemptLogin = new AttemptLogin();
-                    attemptLogin.execute(fullname.getText().toString(),email.getText().toString(),password.getText().toString());
+//                    AttemptLogin attemptLogin = new AttemptLogin();
+//                    attemptLogin.execute(fullname.getText().toString(),email.getText().toString(),password.getText().toString());
 
-                }
+//                }
 
-            }
-        });
+//            }
+//        });
 
-    }
+//    }
 
-    private class AttemptLogin extends AsyncTask<String, String, JSONObject> {
+//    private class AttemptLogin extends AsyncTask<String, String, JSONObject> {
 
-        @Override
+//        @Override
 
-        protected void onPreExecute() {
+//        protected void onPreExecute() {
 
-            super.onPreExecute();
+//            super.onPreExecute();
 
-        }
+//        }
 
-        @Override
+//        @Override
 
-        protected JSONObject doInBackground(String... args) {
-
-
-            String email = args[2];
-            String password = args[1];
-            String name = args[0];
-
-            ArrayList params = new ArrayList();
-            params.add(new BasicNameValuePair("username", name));
-            params.add(new BasicNameValuePair("password", password));
-            if (email.length() > 0)
-                params.add(new BasicNameValuePair("email", email));
-
-            JSONObject json = jsonParser.makeHttpRequest(URL, "POST", params);
+//        protected JSONObject doInBackground(String... args) {
 
 
-            return json;
+//            String email = args[2];
+//            String password = args[1];
+//            String name = args[0];
 
-        }
+//            ArrayList params = new ArrayList();
+//            params.add(new BasicNameValuePair("username", name));
+//            params.add(new BasicNameValuePair("password", password));
+//            if (email.length() > 0)
+//                params.add(new BasicNameValuePair("email", email));
 
-        protected void onPostExecute(JSONObject result) {
+//            JSONObject json = jsonParser.makeHttpRequest(URL, "POST", params);
 
-            // dismiss the dialog once product deleted
-            //Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
 
-            try {
-                if (result != null) {
-                    Toast.makeText(getApplicationContext(), result.getString("message"), Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Unable to retrieve any data from server", Toast.LENGTH_LONG).show();
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+//            return json;
 
-        }
-    }
+//        }
+
+//        protected void onPostExecute(JSONObject result) {
+
+//            // dismiss the dialog once product deleted
+//            //Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
+
+//            try {
+//                if (result != null) {
+//                    Toast.makeText(getApplicationContext(), result.getString("message"), Toast.LENGTH_LONG).show();
+//                } else {
+//                    Toast.makeText(getApplicationContext(), "Unable to retrieve any data from server", Toast.LENGTH_LONG).show();
+//                }
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+
+//        }
+//    }
+
+        //spalshDots lanjutan
+//        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//            Log.d("OnPageScrolled", String.valueOf(position));
+//        }
+
+
+//        public void onPageSelected(int position) {
+//            Log.d("OnPageSelected", String.valueOf(position));
+
+//        }
+
+
+//        public void onPageScrollStateChanged(int state) {
+//            Log.d("PageScrollStateChanged", String.valueOf(state));
+
+//        }
 }
---
+++ SuperFam/app/src/main/java/com/example/windows10/superfam/PagerAdapter.java
@@ -0,0 +1,61 @@
+package com.example.windows10.superfam;
+
+import android.support.v4.view.ViewPager;
+
+/**
+ * Created by Windows 10 on 5/3/2018.
+ */
+
+//public class PagerAdapter extends ViewPager {
+
+   //  viewPager.addOnPageChangeListener(new PagerAdapter() {
+   //     @Override
+   //     public void onPageSelected(int position) {
+
+   //         switch (position) {
+   //             case 0:
+   //                 img_page1.setImageResource(R.drawable.dot_selected);
+   //                 img_page2.setImageResource(R.drawable.dot);
+   //                 img_page3.setImageResource(R.drawable.dot);
+   //                 img_page4.setImageResource(R.drawable.dot);
+   //                 break;
+
+   //             case 1:
+   //                 img_page1.setImageResource(R.drawable.dot);
+   //                 img_page2.setImageResource(R.drawable.dot_selected);
+   //                 img_page3.setImageResource(R.drawable.dot);
+   //                 img_page4.setImageResource(R.drawable.dot);
+   //                 break;
+
+   //             case 2:
+   //                 img_page1.setImageResource(R.drawable.dot);
+   //                 img_page2.setImageResource(R.drawable.dot);
+   //                 img_page3.setImageResource(R.drawable.dot_selected);
+   //                 img_page4.setImageResource(R.drawable.dot);
+   //                 break;
+
+   //             case 3:
+   //                 img_page1.setImageResource(R.drawable.dot);
+   //                 img_page2.setImageResource(R.drawable.dot);
+   //                 img_page3.setImageResource(R.drawable.dot);
+   //                 img_page4.setImageResource(R.drawable.dot_selected);
+   //                 break;
+
+   //             default:
+   //                 break;
+            //}
+
+
+   //     }
+
+   //     @Override
+   //     public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+   //     }
+
+   //     @Override
+   //     public void onPageScrollStateChanged(int arg0) {
+
+   //     }
+   // });
+//}
